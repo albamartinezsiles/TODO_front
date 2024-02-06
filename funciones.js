df0bbebf7337e5981fd3b4487fa79f9e8fb6f58a
Alba <dok-do@hotmail.es>
2024-02-06 19:10:51 +0100

    primera clase con comentarios

--- funciones.js
+++ funciones.js
@@ -1,4 +1,5 @@
 const contenedorTareas = document.querySelector(".tareas");
 
+//Aquí van las tareas que vamos creando
 new Tarea(12,"blablaba",true,contenedorTareas);
 new Tarea(12,"otra",false,contenedorTareas);
--- tarea.js
+++ tarea.js
@@ -1,22 +1,22 @@
 class Tarea{
     constructor(id,textoTarea,estado,contenedor){ //cada tarea necesita... el contenedor es donde lo vamos a soltar con un appendChild
-        this.id = id;
+        this.id = id; //cuando usamos this. significa que estamos obteniendo el valor de la propiedad que le sigue del objeto actual
         this.textoTarea = textoTarea;
-        this.DOM = null; //representa la tarea en la interfaz. Es el componente HTML
-        this.crearComponente(estado,contenedor);
+        this.DOM = null; //representa la tarea en la interfaz. Es el componente HTML. Es el div que contiene la tarea
+        this.crearComponente(estado,contenedor); //esto llama a la función crearComponente y le pasa el estado y el contenedor.
     }
-    crearComponente(estado,contenedor){
-        this.DOM = document.createElement("div");
-        this.DOM.classList.add("tarea"); //también se puede usar className("tarea");
+    crearComponente(estado,contenedor){ //método(funcion para los amigos) de la clase tarea 
+        this.DOM = document.createElement("div"); //en el elemento dom, es decir, la tarea, crea un div
+        this.DOM.classList.add("tarea"); //Añádale la clase tarea
 
         //texto
-        let textoTarea = document.createElement("h2");
-        textoTarea.classList.add("visible");
-        textoTarea.innerHTML = this.textoTarea; //no existe en el scope por eso la creamos otra vez
+        let textoTarea = document.createElement("h2"); //Crea un texto h2
+        textoTarea.classList.add("visible"); //añádele la clase visible
+        textoTarea.innerHTML = this.textoTarea; //Copia el valor de this.textoTarea y colócalo como el contenido interno del elemento HTML textoTarea. Esto podría ser útil, por ejemplo, para actualizar dinámicamente el texto en una página web.Esto no existe en el scope por eso la creamos otra vez
 
         //input
         let inputTarea = document.createElement("input");
-        inputTarea.setAttribute("type","text");
+        inputTarea.setAttribute("type","text"); //añádele atributo tipo texto
         inputTarea.value = this.textoTarea; //el value diga lo mismo que textoTarea
 
         //boton editar
@@ -31,14 +31,14 @@ class Tarea{
 
         //boton estado
         let botonEstado = document.createElement("button");
-        botonEstado.classList.add("estado", estado ? "termianda" : null); //la variable estado es true o false? si es true ponle estado, si no no hagas nada
-        botonEstado.appendChild(document.createElement("span"));
+        botonEstado.classList.add("estado", estado ? "termianda" : null); //la variable estado es true o false? si es true ponle la clase terminada, si no no hagas nada
+        botonEstado.appendChild(document.createElement("span")); //crea un elemento span y mételo dentro de botonEstado(el boton creado)
 
-        this.DOM.appendChild(textoTarea);
+        this.DOM.appendChild(textoTarea); //añádele al div todo esto
         this.DOM.appendChild(inputTarea);
         this.DOM.appendChild(botonEditar);
         this.DOM.appendChild(botonBorrar);
         this.DOM.appendChild(botonEstado);
-        contenedor.appendChild(this.DOM);
+        contenedor.appendChild(this.DOM); //añade el div al contenedor. Esto hace que el div y todos sus hijos aparezcar en el contenedor
     }
 }
